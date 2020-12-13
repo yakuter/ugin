93c9dd5d527fc4aa97567233da8b53877dfa468a
Erhan Yakut <yakuter@gmail.com>
2020-12-13 23:19:02 +0300

    update

--- model/post.go
+++ model/post.go
@@ -37,7 +37,7 @@ type Tag struct {
 /*
 {
     "Name": "Hello World",
-    "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    "Description": "Lorem ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     "Tags": [
         {
             "Name": "lorem",
