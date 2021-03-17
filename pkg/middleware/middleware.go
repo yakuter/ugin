2a58b6ead0dae0ee6ffd8a30f95e8f2877f5c309
Mehmet Fazlı BERKDEMİR <mfberkdemir@gmail.com>
2021-03-18 00:09:18 +0300

    Swagger Add (#6)
    * JWT add
    
    * Swagger add
    
    * gZip add
    
    * Security middleware add
    
    * Limiter middleware add
    
    * Fix swagger
    
    Co-authored-by: Erhan Yakut <yakuter@gmail.com>

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -74,10 +74,12 @@ func Security() gin.HandlerFunc {
 
 		// Feature-Policy
 		c.Writer.Header().Add("Feature-Policy", "microphone 'none'; camera 'none'")
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
+
 		c.Next()
 	}
 }
